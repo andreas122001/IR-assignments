9740cbc2e49a4bfc3a6739d3ded441fd70436940
Andreas Bentzen Winje <andreasbenwin@hotmail.com>
2023-10-31 16:36:10 +0100

    added ranking to indexer

--- assignment-4/indexer.py
+++ assignment-4/indexer.py
@@ -29,9 +29,9 @@ class Parser:
 
 class CustomIndexer:
     def __init__(self, corpus) -> None:
-        self.original_corpus = corpus
+        self.corpus = corpus
         self.preprocessed_corpus = [self.preprocessing(doc) for doc in corpus]
-        self.index = self.gen_idx_block(self.preprocessed_corpus)
+        self.index = self.gen_idx(self.preprocessed_corpus)
         self.parser = Parser()
         
 
@@ -39,12 +39,23 @@ class CustomIndexer:
         corpus = [[doc.split(" ")[block_size*i:block_size*i+block_size] for i in range(len(doc.split(" "))//block_size+1)] for doc in corpus]
         corpus = [list(filter(lambda x: len(x) > 0, doc)) for doc in corpus]
         return corpus
+    
+    def gen_idx(self, corpus):
+        # Initiate the index as a dict('term', dict('doc', num_occ))
+        idx_list = dict([(key, {}) for key in set(" ".join(corpus).split(" "))])
+        for doc_idx, doc in enumerate(corpus, 0):
+            # Increment number of occurrences for each occurrence
+            for term in doc.split(" "):
+                if doc_idx not in idx_list[term].keys():
+                    idx_list[term][doc_idx] = 0
+                idx_list[term][doc_idx] += 1
+        return idx_list
 
     def gen_idx_block(self, corpus, block_size=3):
         # Initiate the index as a dict('term', dict('doc', [block_ids]))
         idx_list = dict([(key, {}) for key in set(" ".join(corpus).split(" "))])
         corpus_blocks = []
-        for doc_idx, doc in enumerate(corpus, 1):
+        for doc_idx, doc in enumerate(corpus, 0):
             # Generate blocks
             blocks = [doc.split(" ")[block_size*i:block_size*i+block_size] for i in range(len(doc.split(" "))//block_size+1)]
             blocks = list(filter(lambda x: len(x)>0, blocks))
@@ -76,28 +87,21 @@ class CustomIndexer:
         new_text = [stemmer.stem(word) for word in new_text] # perform stemming
         new_text = " ".join(new_text)
         return new_text
-    
-    def retrieve_raw_indexes(self, terms):
-        indexes = []
-        for term in terms:
-            if term in self.index:
-                indexes.append(self.index[term])
-        return indexes
-    
+        
     def retrieve_docs(self, term):
         term = self.preprocessing(term)
         if term in self.index:
             return set(self.index[term].keys())
         return set()
     
-    def search_recursive(self, query, doc_set:set=set()):
+    def filter_recursive(self, query, doc_set:set=set()):
         current_context = set()
         operator = None
 
         for term in query:
             if isinstance(term, list):
                 # Recursively process sub-expression
-                sub_result = self.search_recursive(term, doc_set)
+                sub_result = self.filter_recursive(term, doc_set)
                 if operator == 'AND':
                     doc_set.intersection_update(sub_result)
                 elif operator == 'NOT':
@@ -124,22 +128,49 @@ class CustomIndexer:
         
         # After processing all terms and operators in the query, update the document set
         doc_set.update(current_context)
-        
         return doc_set
     
-    def rank_docs(self, docs):
-        return docs
+    def rank_docs(self, docs, query):
+        # query = query.split(" ")
+        # query = filter(lambda x: x.upper() not in ["AND", "OR", "NOT"], query)
+        # Generate vocabulary of all retrieved documents
+        vocab = set(" ".join([self.preprocessed_corpus[doc] for doc in docs]).split(" "))
+
+        # Remove terms that are not in vocabulary from query
+        query = self.preprocessing(query).split(" ")
+        query = list(filter(lambda x: x in vocab, query))
+        query = list(filter(lambda x: x not in ['and', 'or', 'not'], query))
+
+        # Calculate TF-IDF
+        tfs = np.array([[(self.index[term][doc] if doc in self.index[term] else 0) for term in query] for doc in docs])
+        dfs = np.array([len(self.index[term]) for term in query])
+        idfs = np.log2(len(corpus) / dfs)
+        tf_idf = np.sum(tfs*idfs, axis=1)
+
+        doc_scores = {doc: score for doc, score in zip(docs, tf_idf)}
+        doc_scores = dict(sorted(doc_scores.items(), key=lambda x: x[1])[::-1])
+
+        return doc_scores
 
     def search(self, query):
         parsed_query = self.parser.parse_query(query)
-        parsed_results = list(self.search_recursive(parsed_query))
-        ranked_results = list(self.rank_docs(parsed_results)) # TODO: Rank results
+        filtered_results = list(self.filter_recursive(parsed_query))
+        if not filtered_results:
+            return None
+        ranked_results = self.rank_docs(filtered_results, query)
         return ranked_results
 
-corpus = []
-for i in range(1,7):
-    with open(f"./DataAssignment4/Text{i}.txt") as f:
-        corpus.append(f.read())
 
-indexer = CustomIndexer(corpus)
-print(indexer.search("(enjoy AND bear)"))
+if __name__=="__main__":
+    corpus = []
+    for i in range(1,7):
+        with open(f"./DataAssignment4/Text{i}.txt") as f:
+            corpus.append(f.read())
+
+    indexer = CustomIndexer(corpus)
+    results = indexer.search("(claims of duty)")
+    if results:
+        for doc, score in results.items():
+            print(f"ID: {doc}, Score: {score}, Content: {indexer.corpus[doc][:100]}")
+    else:
+        print("No results")
